d4f6a46e26569ee77c08357674008309e4f187b8
Caique Santos <caisbrito2003@gmail.com>
2020-12-08 18:54:06 -0300

    Encapsulamento feito

---
+++ Encapsulamento/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Encapsulamento.Classes;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MasterCard m = new MasterCard();
+            m.NomeTitular = "Caique Santos Brito";
+            m.Numero = "23112003";
+            m.Bandeira = "Visa";
+
+            Console.WriteLine($"O Titular {m.NomeTitular} - Tem o Token: {m.Token} - e sua Bandeira é : {m.Bandeira}");
+        }
+    }
+}
